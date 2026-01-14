a14ece81566ed3f4aa101419a3f2e420f5007ecf
luanSchifini <luanschifini06@gmail.com>
2026-01-13 23:34:01 -0300

    build: add repositories layer

--- app/repositories/weather_repository.py
+++ app/repositories/weather_repository.py
@@ -12,20 +12,27 @@ class WeatherRepository:
         self.db = db
 
     def create_weather_record(self, weather_data: WeatherRequest) -> WeatherRecord:
-        new_entry = WeatherRecord(
+        """
+        Creates a new weather record in the database.
+        """
+        new_weather_record = WeatherRecord(
             city=weather_data.city,
             temperature=weather_data.temperature,
             humidity=weather_data.humidity,
             description=weather_data.description
         )
 
-        self.db.add(new_entry)
+        self.db.add(new_weather_record)
         self.db.commit()
-        self.db.refresh(new_entry)
+        self.db.refresh(new_weather_record)
 
-        return new_entry
+        return new_weather_record
 
     def get_recent_history(self, limit: int = 20) -> list[WeatherRecord]:
+        """
+        Returns the history of the last collections made to the database, sorted by creation date.
+        The default limit is 20.
+        """
         return (
             self.db.query(WeatherRecord)
             .order_by(desc(WeatherRecord.created_at))
--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,7 +1,5 @@
 from .weather_collector import WeatherCollectorService
-from .weather_repository import WeatherRepository
 
 __all__ = [
     "WeatherCollectorService",
-    "WeatherRepository",
 ]
